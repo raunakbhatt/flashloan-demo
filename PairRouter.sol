3b438ea014f6c453cc8257e2c267f75e9c561397
Pancakeswap <100120817+PancakeswapMCL@users.noreply.github.com>
2022-03-25 12:37:40 +0800

    Create PairRouter.sol

---
+++ PairRouter.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+	}
+}
